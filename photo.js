fe27db95c4c351d76699ca5ed524220192d7d385
makqianhui <155415388+makqianhui@users.noreply.github.com>
2024-04-09 20:57:23 +0800

    Add files via upload

---
+++ photo.js
@@ -0,0 +1,80 @@
+const photosInfo = [
+    
+    {
+        cephalogram_url: './WhatsApp Image 2024-04-09 at 6.52.55 PM (1).jpeg',
+        name: 'In The Face Of Love , Space No Effort To Like',
+        introduction: '面 对 热 爱 要 不 遗 余 力 的 喜 欢',
+        size: 'width-11',
+        children: [
+            {
+                children_url: './inside 2.4.jpeg',
+                children_introduction: '',
+            },
+            {
+                children_url: './inside 2.2.jpeg',
+                children_introduction: '',
+            },
+            {
+                children_url: './inside 2.1.jpeg',
+                children_introduction: 'Wishing You A Day Filled With Surprises , Love , And Everything You´ve Been Dreaming Of. 祝 你 的 生 日 充 满 惊 喜 ， 爱 意 和 你 一 直 梦 寐 以 求 的 一 切。',
+            },
+            {
+                children_url: './inside 2.3.jpeg',
+                children_introduction: '',
+            },
+            {
+                children_url: './inside 2.5.jpeg',
+                children_introduction: `Cheets To Another year Of Laughter , Joy , And Incredible Memories. 为 又 一 年 的 欢 笑 ， 快 乐 和 美 好 记 忆 干 杯。`,
+            },
+        ]
+    },
+    {
+        cephalogram_url: './WhatsApp Image 2024-04-09 at 6.52.55 PM.jpeg',
+        name: 'Dancing Through Life Adventures ',
+        introduction: '与 生 活 的 挑 战 共 舞 ',
+        size: 'width-11',
+        children: [
+            {
+                children_url: './inside 3.1.jpeg',
+                children_introduction: '',
+            },
+            {
+                children_url: './inside 3.2.jpeg',
+                children_introduction: `The Past Is Not Worried , The Future Is Not Afraid , The New Year Is Growing , Carefree. 往 事 不 忧 ，未 来 何 惧 ，新 岁 渐 长 ， 无 忧 无 虑。 `,
+            },
+            {
+                children_url: './inside 3.4.jpeg',
+                children_introduction: '',
+            },
+            {
+                children_url: './inside 3.3.jpeg',
+                children_introduction: `Nothing Stirs The Strings Of Your Heart , Everything I Think Is As You Wish. 无 事 绊 心 弦 ， 所 念 皆 如 愿。`,
+            },
+        ]
+    },
+    {
+        cephalogram_url: './WhatsApp Image 2024-04-09 at 6.49.29 PM.jpeg',
+        name: 'The Journey of a girl is a sea of star , not a world of dust',
+        introduction: '少 女 的 征 途 是 星 辰 大 海 而 并 非 烟 尘 人 间',
+        size: 'width-24',
+        children: [
+            {
+                children_url: './inside 4.4.jpeg',
+                children_introduction: `Today Is A Day Where You Can Make Wishes Even Without A Shooting Star. 今 天 是 没 有 流 星 也 可 以 许 愿 的 日 子。`,
+            },
+            {
+                children_url: './WhatsApp Image 2024-04-09 at 7.04.41 PM.jpeg',
+                children_introduction: '',
+            },
+            {
+                children_url: './inside 4.2.jpeg',
+                children_introduction: '',
+            },
+            {
+                children_url: './WhatsApp Image 2024-04-09 at 7.04.41 PM (1).jpeg',
+                children_introduction: '',
+            },
+        ]
+    },
+
+]
